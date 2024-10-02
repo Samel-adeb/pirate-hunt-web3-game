e83c6bcbdd1a16a068cda9c9cf645f26bc6d82b8
Taiwo Joel <oluwatofunmijoel765@gmail.com>
2024-10-02 18:41:05 +0100

    navbar removed

--- app/components/GameNavbar.tsx
+++ app/components/GameNavbar.tsx
@@ -5,19 +5,5 @@
 export function GameNavbar() {
     
 
-    return (
-        <nav className="relative lg:hidden md:hidden xl:hidden bg-[#1A314E]  py-[8.08px]  px-[16.15px] w-full">
-            <div  className="flex items-center justify-center">
-                
-
-                <div className="flex flex-col items-center justfy-center text-center">
-                    <h1 className="text-[16.15px] leading-[19.55px] font-semibold text-white">Pirate Hunt Coin</h1>
-                    <p className="text-[12.11px] leading-[14.66px] font-normal text-white">bot</p>
-                </div>
-
-               
-            </div>
-        </nav>
-
-    )
+    return null;
 }
