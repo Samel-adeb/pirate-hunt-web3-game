141d6fc83654a94727a9f2c458ec259d482ff83b
Samel-adeb <sameladebisola@gmail.com>
2024-10-09 00:40:06 +0100

    modified config files for production

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ module.exports = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http', // Your API URL uses HTTP, so this needs to match
-                hostname: process.env.IMAGE_HOST || '127.0.0.1',//'wealthvalley.site', // Fallback in case IMAGE_HOST isn't defined
-                port: process.env.IMAGE_HOST_PORT || '8000', // Fallback if port isn't defined
+                protocol: 'https', // Your API URL uses HTTP, so this needs to match
+                hostname: process.env.IMAGE_HOST || 'wealthvalley.site', //'127.0.0.1',// Fallback in case IMAGE_HOST isn't defined
+                port: process.env.IMAGE_HOST_PORT || '', // 8000Fallback if port isn't defined
                 pathname: '/**', // Matches all paths under this domain
             },
         ],
