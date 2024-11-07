0f69293524a569d69d55ab28f00f9a46d96d963f
Samel-adeb <sameladebisola@gmail.com>
2024-11-08 00:43:09 +0100

    http to https

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     images: {
         remotePatterns: [
             {
-                protocol: 'http', // Your API URL uses HTTP, so this needs to match
+                protocol: 'https', // Your API URL uses HTTP, so this needs to match
                 hostname: process.env.NEXT_PUBLIC_IMAGE_HOST, //'127.0.0.1',// Fallback in case IMAGE_HOST isn't defined
                 port: process.env.NEXT_PUBLIC_IMAGE_HOST_PORT, // 8000Fallback if port isn't defined
                 pathname: '/**', // Matches all paths under this domain
